30499b7cf2458310ff8d697976fc04ecc0a56db2
Shasha Sami <shasha.sami91199@gmail.com>
2020-05-30 11:34:56 +0530

    Update ProducerAsync.java

--- ProducerAsync.java
+++ ProducerAsync.java
@@ -5,19 +5,19 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.util.Properties;
 public class ProducerAsync
 {
-    public static void main(String[] args) {
-        Properties props = new Properties();
+    public static void main(String[] args){
+        Properties props=new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         Producer<String, String> producer = new KafkaProducer<>(props);
         ProducerRecord<String, String> record = new ProducerRecord<>("producer", "String Key", "Hello! Async Send");
         try {
-            producer.send(record, (recordMetadata, e) -> {
+            producer.send(record,(recordMetadata, e) -> {
                 if (e != null)
                     e.printStackTrace();
             });
-        } catch (Exception e) {
+        } catch(Exception e) {
             e.printStackTrace();
         }
         producer.close();
